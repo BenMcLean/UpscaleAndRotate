251a584d3d82817b179b0b2901555a8f281b26ea
BenMcLean <mclean.ben@gmail.com>
2024-12-06 17:44:31 -0600

    Scaling factors should also be test parameters

--- UpscaleAndRotate.Test/UpscaleAndRotateTest.cs
+++ UpscaleAndRotate.Test/UpscaleAndRotateTest.cs
@@ -5,8 +5,8 @@ namespace UpscaleAndRotate.Test;
 public class UpscaleAndRotateTest
 {
 	[Theory]
-	[InlineData(160, 319, 64, "rotated.gif")]
-	public void RotateTest(ushort width, ushort height, ushort numFrames, string path)
+	[InlineData(160, 319, 1, 1, 64, "rotated.gif")]
+	public void RotateTest(ushort width, ushort height, byte scaleX, byte scaleY, ushort numFrames, string path)
 	{
 		byte[] texture = new byte[width * height << 2].DrawBoundingBox(width);
 		byte[][] frames = ImageMaker.SameSize(
@@ -16,8 +16,8 @@ public class UpscaleAndRotateTest
 						rotatedWidth: out ushort rotatedWidth,
 						rotatedHeight: out ushort _,
 						radians: Math.Tau * ((double)i / numFrames),
-						scaleX: 1,
-						scaleY: 1,
+						scaleX: scaleX,
+						scaleY: scaleY,
 						width: width), rotatedWidth))],
 			width: out ushort newWidth,
 			height: out _);
